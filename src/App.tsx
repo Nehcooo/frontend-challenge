84a02305d71c5e39912dbffbe3096ce8b14582a9
Mehdi <itsnehco@gmail.com>
2025-08-01 23:21:22 +0200

    update: app.tsx, add mui theme & components loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+declare module '@mui/material/styles' {
+	interface PaletteOptions {
+		custom: {
+			border: string;
+			iconButtonBorder: string;
+		};
+	}
 }
 
-export default App
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#3F5EFB",
+		},
+		secondary: {
+			main: "#F59000",
+		},
+		text: {
+			primary: "#000000",
+			secondary: "#FFFFFF",
+		},
+		custom: {
+			border: "#E8EAED",
+			iconButtonBorder: "#DADCE0",
+		},
+	},
+});
+
+function App() {
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+
+				<Box sx={{
+					display: "flex",
+					flexDirection: "row",
+					width: "100%",
+					height: "100vh",
+				}}>
+					<Sidebar />
+					<Header />
+				</Box>
+			</ThemeProvider>
+		</>
+	)
+}
+
+export default App;
